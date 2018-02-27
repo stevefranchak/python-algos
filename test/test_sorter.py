a3d3021142fc01513a62d1b3725a85e15eba259c
Steve Franchak <stevefranchak@gmail.com>
2018-02-27 11:11:24 -0500

    Neglected to test negative integers.

--- test/test_sorter.py
+++ test/test_sorter.py
@@ -19,7 +19,8 @@ from sorter import bubble_sort, selection_sort, insertion_sort
     ([8, 3, 4, 2], [2, 3, 4, 8]),
     ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]),
     ([5, 4, 3, 2, 1], [1, 2, 3, 4, 5]),
-    ([65, 11, 92, 14, 8, 777, 21, 11, 786], [8, 11, 11, 14, 21, 65, 92, 777, 786])
+    ([65, 11, 92, 14, 8, 777, 21, 11, 786], [8, 11, 11, 14, 21, 65, 92, 777, 786]),
+    ([-7, 8, -5, 0, 9, -5, -10, 1], [-10, -7, -5, -5, 0, 1, 8, 9])
 ])
 def test_sorter_functions(func, array_to_sort, expected_sorted_array):
     """Test each sorting algorithm implementation against the same set
